68715cd8a7280f36be307d32a3b66c81a4d3a7da
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-07 16:40:31 +0200

    add all trip

--- backend/controllers/Trip.js
+++ backend/controllers/Trip.js
@@ -18,3 +18,25 @@ const getAllTrip = (req, res) => {
     });
   };
   
+  const creatNewTrip =(req,res)=>{
+    const {tripName,from,to,Image,Price,numbersite}=req.body
+    const userId=req.token.userId;
+    const query = `INSERT INTO articles (tripName,from,to,Image,Price,numbersite,userId) VALUES (?,?,?,?,?,?,?);`;
+const data=[tripName,from,to,Image,Price,numbersite,userId]
+connection.query(query,data,(err,result)=>{
+  if (err) {
+    res.status(500).json({
+      success: false,
+      massage: "Server error",
+      err: err,
+    });
+  }
+  // result are the data returned by mysql server
+  res.status(200).json({
+    success: true,
+    massage: "Success Trip created",
+    results: result,
+  });
+
+})
+  }
