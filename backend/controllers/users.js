f51d5299cfb6e1a71cc2799f31bf723a77448b7f
majdinazzal <majdiahmadnazzal@gmail.com>
2022-03-07 11:58:05 +0300

    users

---
+++ backend/controllers/users.js
@@ -0,0 +1,34 @@
+const connection = require("../database/db");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10;
+
+const createNewUser = async (req, res) => {
+  const { Username, Phone_number, password, email } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+  const query = `INSERT INTO user (Username, Phone_number, Password, email) VALUES (?,?,?,?)`;
+  const data = [Username, Phone_number, encryptedPassword, email];
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      res.status(409).json({
+        success: false,
+        massage: "The email already exists",
+        err: err,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      massage: "User has been added successfully",
+      results: results,
+    });
+  });
+};
+
+
+module.exports = {
+  createNewUser,
+};
