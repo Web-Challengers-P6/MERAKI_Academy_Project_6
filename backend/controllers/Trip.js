6a9c7f26f99a698ecb1aa076f3fa9fa7bd23073a
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-07 16:39:24 +0200

    get all trip

--- backend/controllers/Trip.js
+++ backend/controllers/Trip.js
@@ -1,1 +1,20 @@
 const connection = require("../database/db");
+
+
+const getAllTrip = (req, res) => {
+    const query = `SELECT * FROM trip WHERE is_deleted=0`;
+    connection.query(query, (err, result) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ success: false, massage: "server error", err: err });
+      }
+      res.status(200).json({
+          success: true,
+          massage: "All the trip",
+          results: result,
+        });
+    
+    });
+  };
+  
