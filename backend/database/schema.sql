14d78e7379190620a94aa39cce767c20dc53b4d6
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-08 12:11:33 +0200

    edit on table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,38 +1,38 @@
--- CREATE DATABASE PROJECT6 ;
+CREATE DATABASE PROJECT6 ;
 USE  PROJECT6;
 
--- create table user (
--- id INT AUTO_INCREMENT NOT NULL,
--- Username VARchar (255) UNIQUE,
--- Phone_number INT,
--- email VARchar (255) UNIQUE,
--- Password VARchar (255),
--- profileImg VARCHAR(255),
--- Primary Key (id)
--- );
+create table user (
+id INT AUTO_INCREMENT NOT NULL,
+Username VARchar (255) UNIQUE,
+Phone_number INT,
+email VARchar (255) UNIQUE,
+Password VARchar (255),
+profileImg VARCHAR(255),
+Primary Key (id)
+);
 
--- create table trip (
--- id INT AUTO_INCREMENT NOT NULL,
--- tripName VARchar (255),
--- TRIPfrom  VARchar (255),
--- TRIPto VARchar (255),
--- Image VARchar (255),
--- driverId INT,
--- Price INT,
--- numbersite INT,
--- Category VARchar (255),
--- softDelete INT DEFAULT 0,
--- FOREIGN KEY (driverId) REFERENCES user(id),
--- Primary Key (driverId)
--- );
+create table trip (
+id INT AUTO_INCREMENT NOT NULL,
+tripName VARchar (255),
+TRIPfrom  VARchar (255),
+TRIPto VARchar (255),
+Image VARchar (255),
+driverId INT,
+Price INT,
+numbersite INT,
+Category VARchar (255),
+softDelete INT DEFAULT 0,
+FOREIGN KEY (driverId) REFERENCES user(id),
+Primary Key (id)
+);
 
 create table rider (
 id INT AUTO_INCREMENT NOT NULL,
 riderid INT,
-driverId INT,
+tripid INT,
 softDelete INT DEFAULT 0,
 FOREIGN KEY (riderid) REFERENCES user(id),
-FOREIGN KEY (driverId) REFERENCES trip(driverId),
+FOREIGN KEY (tripid) REFERENCES trip(id),
 Primary Key (id)
 
 );
