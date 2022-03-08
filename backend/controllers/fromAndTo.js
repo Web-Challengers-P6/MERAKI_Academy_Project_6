d4ef4098204ea05569574ae32f3d05f6f9eec796
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-03-09 02:53:06 +0300

    from func is done

--- backend/controllers/fromAndTo.js
+++ backend/controllers/fromAndTo.js
@@ -1,2 +1,20 @@
 const Connection = require("mysql2/typings/mysql/lib/Connection");
 const db = require("../database/db");
+const from = (req, res) => {
+  const fromPoint = req.body;
+  const query = `select * from trip where from=${fromPoint}`;
+  Connection.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(404)
+        .json({ success: false, message: "no results where found", err });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "{from} Point Info",
+        result: result,
+      });
+    }
+  });
+};
