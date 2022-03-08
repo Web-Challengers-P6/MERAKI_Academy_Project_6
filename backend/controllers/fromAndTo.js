41e5caef24622223cda65d7ade76774f0a1d91f8
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-03-09 02:53:45 +0300

    the TO func is done

--- backend/controllers/fromAndTo.js
+++ backend/controllers/fromAndTo.js
@@ -18,3 +18,22 @@ const from = (req, res) => {
     }
   });
 };
+
+const to = (req, res) => {
+    const toPoint = req.body;
+    const query = `select * from trip where from=${toPoint}`;
+    Connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(404)
+          .json({ success: false, message: "no results where found", err });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "{to} Point Info",
+          result: result,
+        });
+      }
+    });
+  };
