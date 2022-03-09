f483702348b651b3b4c932c631396cd99dfa7014
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-09 06:05:15 +0200

    login component

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -6,3 +6,19 @@ import { useSelector, useDispatch } from "react-redux";
 import { login } from "../reducers/login";
 
 //===============================================================
+const Login = () => {
+    const history = useNavigate();
+  
+    // ---------------------------------------------
+    const state = useSelector((state) => {
+      return { isLoggedIn: state.loginReducer.isLoggedIn };
+    });
+  
+    const dispatch = useDispatch();
+    // ---------------------------------------------
+  
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [password, setPassword] = useState("");
+    const [status, setStatus] = useState(false);
+  
