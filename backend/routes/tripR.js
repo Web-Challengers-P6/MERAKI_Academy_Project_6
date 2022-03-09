44ebccffac92fc8ed9591beaa148aaae41a0acd7
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-09 11:03:48 +0300

    trip router

--- backend/routes/tripR.js
+++ backend/routes/tripR.js
@@ -2,3 +2,9 @@ const express = require("express");
 
 const getAllTrip = require("../controllers/Trip");
 const creatNewTrip = require("../controllers/Trip");
+
+const tripRouter = express.Router();
+tripRouter.get("/all", getAllTrip);
+tripRouter.post("/createNewTrip", creatNewTrip);
+
+
