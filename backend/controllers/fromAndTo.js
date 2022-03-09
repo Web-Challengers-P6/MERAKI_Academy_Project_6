1999b2ee5060fa337f5f807e307808fbb231875f
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-09 11:57:09 +0300

    fixing filter func

--- backend/controllers/fromAndTo.js
+++ backend/controllers/fromAndTo.js
@@ -1,8 +1,10 @@
 const db = require("../database/db");
-const from = (req, res) => {
+const Connection = require("../database/db");
+const fromAndTo = (req, res) => {
   const fromPoint = req.body;
-  const query = `select * from trip where from=${fromPoint}`;
-  Connection.query(query, (err, result) => {
+  const query = `select * from trip where TRIPfrom=?`;
+  const data = [fromPoint];
+  Connection.query(query, data, (err, result) => {
     if (err) {
       console.log(err);
       res
@@ -20,14 +22,16 @@ const from = (req, res) => {
 
 const to = (req, res) => {
   const toPoint = req.body;
-  const query = `select * from trip where from=${toPoint}`;
-  Connection.query(query, (err, result) => {
+  const query = `select * from trip where TRIPto=?`;
+  const data = [toPoint];
+  Connection.query(query, data, (err, result) => {
     if (err) {
       console.log(err);
       res
         .status(404)
         .json({ success: false, message: "no results where found", err });
     } else {
+      console.log(result);
       res.status(200).json({
         success: true,
         message: "{to} Point Info",
