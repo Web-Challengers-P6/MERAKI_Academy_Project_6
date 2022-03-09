cf746ebf6fee7e90e56b93c28c7029377c765154
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-09 05:58:08 +0200

    add new user

--- frontend/src/components/regester/regester.js
+++ frontend/src/components/regester/regester.js
@@ -19,4 +19,32 @@ const Register = () => {
     const [message, setMessage] = useState("");
     const [status, setStatus] = useState(false);
   
-  
+    // =================================================================
+
+    const addNewUser = async (e) => {
+        e.preventDefault();
+        try {
+          const result = await axios.post("http://localhost:5000/users", {
+            firstName,
+            lastName,
+            age,
+            country,
+            email,
+            password,
+            role_id,
+          });
+          if (result.data.success) {
+            setStatus(true);
+            setMessage("The user has been created successfully");
+          } else throw Error;
+        } catch (error) {
+          setStatus(false);
+          if (error.response && error.response.data) {
+            return setMessage(error.response.data.message);
+          }
+          setMessage("Error happened while register, please try again");
+        }
+      };
+    
+      // =================================================================
+    
