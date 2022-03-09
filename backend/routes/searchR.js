c82db95d669413031bcf48b61dfec3046630eb6a
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-03-09 03:25:05 +0300

    set up the routers in the server

--- backend/routes/searchR.js
+++ backend/routes/searchR.js
@@ -3,4 +3,4 @@ const search = require("../controllers/search");
 const searchRouter = express.Router();
 //main route is /search
 searchRouter.get("/:Product_Name", search);
-module.exports = searchRouter;
+module.exports = { searchRouter };
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
+//Import Routers
+const profileRouter = require("./routes/profileR");
+const fromAndToFilterRouter = require("./routes/fromAndtoR");
+const searchRouter = require("./routes/searchR");
+
+//Routes Middleware
+app.use("/profile", profileRouter);
+app.use("/search", searchRouter);
+app.use("/filter", fromAndToFilterRouter);
 
 app.listen(PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${PORT}`);
