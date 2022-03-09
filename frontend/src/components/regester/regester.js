6f53c19f47309c17ec9650138272077905a5da78
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-09 05:56:03 +0200

    regester react

--- frontend/src/components/regester/regester.js
+++ frontend/src/components/regester/regester.js
@@ -3,3 +3,20 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 
 import { useSelector} from "react-redux";
+//=====================================================
+const Register = () => {
+    const state = useSelector((state) => {
+      return { isLoggedIn: state.loginReducer.isLoggedIn };
+    });
+  
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [age, setAge] = useState(0);
+    const [country, setCountry] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const role_id = "1";
+    const [message, setMessage] = useState("");
+    const [status, setStatus] = useState(false);
+  
+  
