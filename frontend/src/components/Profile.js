43d19fdc4c21fe62854ec3959a7682a5907d7886
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-10 12:52:17 +0300

    profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,104 +1,112 @@
 import React from "react";
 import axios from "axios";
 const Profile = () => {
-  const getAllInformationFE = () => {
-    axios.get(
-        `http://localhost:5000/product/byuser`,
-        userId
-      );
-
-
-  };
+  //   const userId = 1;
+  //   const getAllInformationFE = () => {
+  //     axios.get(`http://localhost:5000/profile/:${userId}`);
+  //   };
   return (
-    <div class="container rounded bg-white mt-5 mb-5">
-      <div class="row">
-        <div class="col-md-3 border-right">
-          <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-            <img
-              class="rounded-circle mt-5"
-              width="150px"
-              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            />
-            <span class="font-weight-bold">Edogaru</span>
-            <span class="text-black-50">edogaru@mail.com.my</span>
-            <span> </span>
+    <div>
+      <div className="container rounded bg-white mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-3 border-right">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+              <img
+                className="rounded-circle mt-5"
+                width="150px"
+                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              />
+              <span className="font-weight-bold">Edogaru</span>
+              <span className="text-black-50">edogaru@mail.com.my</span>
+              <span> </span>
+            </div>
           </div>
-        </div>
-        <div class="col-md-5 border-right">
-          <div class="p-3 py-5">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-              <h4 class="text-right">Profile Settings</h4>
-            </div>
-            <div class="row mt-2">
-              <div class="col-md-6">
-                <label class="labels">Name</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="first name"
-                />
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <h4 className="text-right">Profile Settings</h4>
               </div>
-              <div class="col-md-6">
-                <label class="labels">Last Name</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Last Name"
-                />
+              <div className="row mt-2">
+                <div className="col-md-6">
+                  <label className="labels">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="first name"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="labels">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Last Name"
+                  />
+                </div>
               </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col-md-12">
-                <label class="labels">Mobile Number</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="enter phone number"
-                />
-              </div>
+              <div className="row mt-3">
+                <div className="col-md-12">
+                  <label className="labels">Mobile Number</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="enter phone number"
+                  />
+                </div>
 
-              <div class="col-md-6">
-                <label class="labels">Country</label>
-                <input type="text" class="form-control" placeholder="country" />
+                <div className="col-md-6">
+                  <label className="labels">Country</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="country"
+                  />
+                </div>
+              </div>
+              <div className="mt-5 text-center">
+                <button
+                  className="btn btn-primary profile-button"
+                  type="button"
+                >
+                  Save Profile
+                </button>
               </div>
             </div>
-            <div class="mt-5 text-center">
-              <button class="btn btn-primary profile-button" type="button">
-                Save Profile
-              </button>
-            </div>
           </div>
-        </div>
-        <div class="col-md-4">
-          <div class="p-3 py-5">
-            <div class="d-flex justify-content-between align-items-center experience">
-              <span>Edit Experience</span>
-              <span class="border px-3 p-1 add-experience">
-                <i class="fa fa-plus"></i>&nbsp;Experience
-              </span>
-            </div>
-            <br />
-            <div class="col-md-12">
-              <label class="labels">Experience in Designing</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="experience"
-              />
-            </div>{" "}
-            <br />
-            <div class="col-md-12">
-              <label class="labels">Additional Details</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="additional details"
-              />
+          <div className="col-md-4">
+            <div className="p-3 py-5">
+              <div className="d-flex justify-content-between align-items-center experience">
+                <span>Edit Experience</span>
+                <span className="border px-3 p-1 add-experience">
+                  <i className="fa fa-plus"></i>&nbsp;Experience
+                </span>
+              </div>
+              <br />
+              <div className="col-md-12">
+                <label className="labels">Experience in Designing</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="experience"
+                />
+              </div>{" "}
+              <br />
+              <div className="col-md-12">
+                <label className="labels">Additional Details</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="additional details"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      {/* <button onClick={getAllInformationFE}>getAllInformation</button> */}
     </div>
   );
 };
 export default Profile;
+
+//working on the get all information function
