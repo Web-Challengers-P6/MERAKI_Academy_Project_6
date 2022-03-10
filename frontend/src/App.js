13297da12ac2cffdc385f5e3a9e558c70399049b
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-10 12:42:48 +0300

    importing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+import "./components/Navbar.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/Navbar";
+import Profile from "./components/Profile";
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/home" element={<Home />} />
-        {/* <Route path="/navbar" element={<NavBar />} /> */}
+        <Route path="/profile" element={<Profile />} />
       </Routes>{" "}
     </>
   );
