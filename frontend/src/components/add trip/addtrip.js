2158c91dca1b4a232d032dff3a1d18bd33b4f951
majdinazzal <majdiahmadnazzal@gmail.com>
2022-03-10 12:46:47 +0300

    ddtrip front

--- frontend/src/components/add trip/addtrip.js
+++ frontend/src/components/add trip/addtrip.js
@@ -2,8 +2,8 @@ import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
-import { addTrips } from "../";///MERAKI_Academy/Projects/Project 6/MERAKI_Academy_Project_6/frontend/src/reducers/trips/index
+
+
 
 
 
@@ -13,26 +13,20 @@ const NewTrip = () => {
   
   const history = useNavigate();
 
-  const state = useSelector((state) => {
-    return {
-      token: state.loginReducer.token,
-      isLoggedIn: state.loginReducer.isLoggedIn,
-    };
-  });
+  
 
-  const { token, isLoggedIn } = state;
+  // const { token, isLoggedIn } = state;
 
-  const dispatch = useDispatch();
+  
 
   const [title, setTitle] = useState("");
-  // const [description, setDescription] = useState("");
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState(false);
   const [tripName, settripName] = useState("");
-  const [TRIPfrom, setTRIPfrom] = useState();
+  const [TRIPfrom, setTRIPfrom] = useState("");
   const [TRIPto, setTRIPto] = useState("");
-  const [numbersite, setnumbersite] = useState("");
-  const [Price, setPrice] = useState("");
+  const [numbersite, setnumbersite] = useState();
+  const [Price, setPrice] = useState();
   
 
   const createNewTrip = async (e) => {
@@ -46,7 +40,8 @@ const NewTrip = () => {
         Price
       };
       const result = await axios.post(
-        "http://localhost:5000/createNewTrip",
+        "http://localhost:5000/trip/createNewTrip",
+        trip,
         {
         tripName,
         TRIPfrom,
@@ -55,22 +50,14 @@ const NewTrip = () => {
         Price
         },
         {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+          // headers: {
+          //   Authorization: `Bearer ${token}`,
+          // },
         }
       );
       if (result.data.success) {
         setStatus(true);
-        dispatch(
-          addTrips({
-            tripName,
-            TRIPfrom,
-            TRIPto,
-            numbersite,
-            Price
-          })
-        );
+       
         setMessage("The trip has been created successfully");
       }
     } catch (error) {
@@ -83,11 +70,11 @@ const NewTrip = () => {
 
   //
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      history("/login");
-    }
-  });
+  // useEffect(() => {
+  //   if (!isLoggedIn) {
+  //     history("/login");
+  //   }
+  // });
 
  
   return (
@@ -102,24 +89,24 @@ const NewTrip = () => {
         <br />
         <textarea
           placeholder="Trip from"
-          onChange={(e) => TRIPfrom(e.target.value)}
+          onChange={(e) => setTRIPfrom(e.target.value)}
         ></textarea>
         <br />
         <textarea
           placeholder="Trip to"
-          onChange={(e) => TRIPto(e.target.value)}
+          onChange={(e) => setTRIPto(e.target.value)}
         ></textarea>
         <br />
         <br />
         <textarea
           placeholder="Number of passengers"
-          onChange={(e) => numbersite(e.target.value)}
+          onChange={(e) => setnumbersite(e.target.value)}
         ></textarea>
         <br />
         <br />
         <textarea
           placeholder="Price per passenger"
-          onChange={(e) => Price(e.target.value)}
+          onChange={(e) => setPrice(e.target.value)}
         ></textarea>
         <br />
         <button>Create New Trip</button>
@@ -142,3 +129,5 @@ const NewTrip = () => {
 };
 
 export default NewTrip;
+
+
