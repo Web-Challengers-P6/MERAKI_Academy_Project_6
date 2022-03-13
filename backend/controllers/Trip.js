43e2244f83699992683c3224cce9694aedd80da9
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-13 12:02:31 +0300

    update the trip func

--- backend/controllers/Trip.js
+++ backend/controllers/Trip.js
@@ -45,14 +45,24 @@ const creatNewTrip = (req, res) => {
     const query = `insert into trip (tripName,TRIPfrom,TRIPto,Image,Price,numbersite) values (?,?,?,?,?,?) where and id=? `;
     const data = [tripName, TRIPfrom, TRIPto, Image, Price, numbersite];
 
-    connection.query(query,data,(err,result)=>{
-
-if (err) {
-  console.log(err);
-  res.status(200).json({success:true,message:"did not update the trip`s information "})}
-
-
-    })
+    connection.query(query, data, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({
+          success: false,
+          message: "did not update the trip`s information ",
+          result: result,
+        });
+      } else {
+        console.log(result);
+        res
+          .status(200)
+          .json({
+            success: true,
+            message: "trip`s information were updated successfuly",
+          });
+      }
+    });
   };
 };
 module.exports = { creatNewTrip, getAllTrip };
