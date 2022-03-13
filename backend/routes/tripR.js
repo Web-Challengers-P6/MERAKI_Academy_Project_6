aef117394342dd59be050b76f8dbb538cb623a9e
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-13 12:07:30 +0300

    update the trip (router)

--- backend/routes/tripR.js
+++ backend/routes/tripR.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
 const { getAllTrip } = require("../controllers/Trip");
-const { creatNewTrip } = require("../controllers/Trip");
+const { creatNewTrip, updateTrip } = require("../controllers/Trip");
 const authentication = require("../middleware/authentication");
 
 const tripRouter = express.Router();
 //main route is /trip
 tripRouter.get("/all", getAllTrip);
 tripRouter.post("/createNewTrip", authentication, creatNewTrip);
-
-
+tripRouter.put();
 
 module.exports = { tripRouter };
