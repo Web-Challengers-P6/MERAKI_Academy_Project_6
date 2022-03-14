ceca7bc377f44b53336c68e96ca29029ab34d781
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-14 08:31:47 +0300

    wrapping the APP with the provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from  "react-router-dom";
+
+import { Provider } from "react-redux";
+
+import store from "./reducers/store";
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter> ,
+  <Router>
+    {/* <UserContext.Provider value={token}> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    {/* </UserContext.Provider> */}
+  </Router>,
   document.getElementById("root")
 );
-
