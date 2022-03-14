6b4521fc9f8720c789ad39ebbb2c5a37b79cf671
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-14 10:00:09 +0300

    redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,22 +8,22 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+//=============================================
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import { Provider } from "react-redux";
 
-import { Provider } from "react-redux";
+// import store from "./reducers/store";
 
-import store from "./reducers/store";
-
-ReactDOM.render(
-  <Router>
-    {/* <UserContext.Provider value={token}> */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-    {/* </UserContext.Provider> */}
-  </Router>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Router>
+//     {/* <UserContext.Provider value={token}> */}
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//     {/* </UserContext.Provider> */}
+//   </Router>,
+//   document.getElementById("root")
+// );
