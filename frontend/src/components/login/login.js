9177332f786188076e06fc38161b2ceaea5dd6c7
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-14 09:09:37 +0200

    make desigen

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,67 +1,88 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, createContext } from "react";
 import { useNavigate } from "react-router-dom";
+import "./login.css";
+import Swal from "sweetalert2";
 
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../reducers/login";
-
+import { login } from "../../reducers/login";
 //===============================================================
+export const UserContext = createContext();
 const Login = () => {
+  const history = useNavigate();
+
+  // ---------------------------------------------
+  const state = useSelector((state) => {
+    return { isLoggedIn: state.loginReducer.isLoggedIn };
+  });
+
+  const dispatch = useDispatch();
+  // ---------------------------------------------
+  //hi
+  const [Username, setUsername] = useState("");
+  const [Phone_number, setPhone_number] = useState(0);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const token = localStorage.getItem("token");
+  const User = localStorage.getItem("User");
 
 
+  const [status, setStatus] = useState(false);
+  // const [saveToken, setSaveToken] = useState("");
+  //===============================================================
 
+  const loginUser = async (e) => {
+    e.preventDefault();
 
-const y = 2
+    try {
+      const res = await axios.post("http://localhost:5000/login", {
+        email,
+        password,
+      });
+      if (res.data) {
+        console.log(res.data)
+        setMessage("true");
+        localStorage.setItem("User", res.data.results.id);
+        localStorage.setItem("token", res.data.token)
 
-    const history = useNavigate();
-  //
-    // ---------------------------------------------
-    const state = useSelector((state) => {
-      return { isLoggedIn: state.loginReducer.isLoggedIn };
-    });
-  
-    const dispatch = useDispatch();
-    // ---------------------------------------------
-  
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [password, setPassword] = useState("");
-    const [status, setStatus] = useState(false);
-    //===============================================================
+        dispatch(login(res.data.token));
+      } else throw Error;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Login, please try again");
+    }
+  };
+  const addNewUser = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:5000/users", {
+        Username,
+        Phone_number,
+        email,
+        password,
+      });
+      if (result.data.success) {
+        setStatus(true);
+        setMessage("The user has been created successfully");
+      } else throw Error;
+    } catch (error) {
+      setStatus(false);
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while register, please try again");
+    }
+  };
 
-    const loginUser = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const res = await axios.post("http://localhost:5000/login", {
-            email,
-            password,
-          });
-          if (res.data.success) {
-            setMessage("");
-            localStorage.setItem("token", res.data.token);
-            // saveToken(res.data.token);
-            dispatch(login(res.data.token));
-          } else throw Error;
-        } catch (error) {
-          if (error.response && error.response.data) {
-            return setMessage(error.response.data.message);
-          }
-          setMessage("Error happened while Login, please try again");
-        }
-      };
-    
-      //===============================================================
-    
-      useEffect(() => {
-        if (state.isLoggedIn) {
-          history("/dashboard");
-        }
-        else
-        history("/login")
-      }, [state.isLoggedIn]);
-    
-      //===============================================================
+  useEffect(() => {
+    if (state.isLoggedIn) {
+      history("/Home");
+    } else history("/login");
+  }, [state.isLoggedIn]);
+
       return (
         <>
           <div className="Form">
