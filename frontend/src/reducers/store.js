d8cb6a501992ed9e943056fbca3eefcf88eb0fb4
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-14 07:54:42 +0300

    creating the store in redux

--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -1,1 +1,7 @@
 import { createStore, combineReducers } from "redux";
+import login from "./trips/loginReducer/index";
+const reducers = combineReducers({ login });
+
+const store = createStore(reducers);
+
+export default store;
