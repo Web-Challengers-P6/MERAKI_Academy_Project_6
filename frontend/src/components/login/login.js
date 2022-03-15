72814646e2908cafed27c630e403b014cc4b55cc
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-15 10:31:46 +0300

    bugFix

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState, useEffect, createContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
 
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../../reducers/login";
+import loginReducer from "../../reducers/loginReducer";
 //===============================================================
 export const UserContext = createContext();
 const Login = () => {
@@ -27,7 +27,6 @@ const Login = () => {
   const token = localStorage.getItem("token");
   const User = localStorage.getItem("User");
 
-
   const [status, setStatus] = useState(false);
   // const [saveToken, setSaveToken] = useState("");
   //===============================================================
@@ -41,12 +40,12 @@ const Login = () => {
         password,
       });
       if (res.data) {
-        console.log(res.data)
+        console.log(res.data);
         setMessage("true");
         localStorage.setItem("User", res.data.results.id);
-        localStorage.setItem("token", res.data.token)
+        localStorage.setItem("token", res.data.token);
 
-        dispatch(login(res.data.token));
+        dispatch(loginReducer(res.data.token));
       } else throw Error;
     } catch (error) {
       if (error.response && error.response.data) {
@@ -82,76 +81,124 @@ const Login = () => {
       history("/Home");
     } else history("/login");
   }, [state.isLoggedIn]);
-  const signUpButton = document.getElementById('signUp');
-  const signInButton = document.getElementById('signIn');
-  const container = document.getElementById('container');
-  
-  signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-  });
-  
-  signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-  });
-      return (
-        <>
-<div class="container" id="container">
-	<div class="form-container sign-up-container">
-		<form action="#" onSubmit={addNewUser}>
-			<h1>Create Account</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name"   onChange={(e) => setUsername(e.target.value)} />
-			<input type="email" placeholder="Email"  onChange={(e) => setEmail(e.target.value)} />
-			<input type="password" placeholder="Password"  onChange={(e) => setPassword(e.target.value)} />
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form action="#" onSubmit={loginUser}>
-			<h1>Sign in</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-			<input type="password" placeholder="Password"  onChange={(e) => setPassword(e.target.value)} />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
 
-<footer>
-	<p>
-		Created with <i class="fa fa-heart"></i> by
-		<a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-		- Read how I created this and how you can join the challenge
-		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-	</p>
-</footer>        </>
-      );
-    };
-    
-    export default Login;
-    
+  //===============================================================
+
+  return (
+    <>
+      <div className="bodylogin">
+        <div class="main">
+          <input className="inp" type="checkbox" id="chk" aria-hidden="true" />
+
+          <div class="signup">
+            <form onSubmit={addNewUser}>
+              <label className="lable" for="chk" aria-hidden="true">
+                Sign up
+              </label>
+              <input
+                className="inp"
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                name="txt"
+                placeholder="User name"
+                required=""
+              />
+              <input
+                className="inp"
+                onChange={(e) => setPhone_number(e.target.value)}
+                type="number"
+                name="phone"
+                placeholder="phone Number"
+                required=""
+              />
+              <input
+                className="inp"
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                placeholder="Email"
+                required=""
+              />
+              <input
+                className="inp"
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required=""
+              />
+              {/* <button
+                onClick={() => {
+                  Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Account successfully created",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                }}
+                className="btnsignup"
+              > */}
+                {/* Sign up */}
+              {/* </button> */}
+            </form>
+          </div>
+
+          <div class="login">
+            <form>
+              <label className="lable" for="chk" aria-hidden="true">
+                Login
+              </label>
+              <input
+                className="inp"
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                placeholder="Email"
+                required=""
+              />
+              <input
+                className="inp"
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required=""
+              />
+              <button className="btnlogin" onClick={loginUser}>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="Form">
+        <p className="Title">Login:</p>
+        <form onSubmit={loginUser}>
+          <br />
+
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button>Login</button>
+        </form>
+
+        {status
+          ? message && <div className="SuccessMessage">{message}</div>
+          : message && <div className="ErrorMessage">{message}</div>} */}
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Login;
