78574f989349b798bb6de95dba553e9268667d3d
majdinazzal <majdiahmadnazzal@gmail.com>
2022-03-16 02:12:24 +0300

    home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 // import AllTrips from "./all trips/alltrips"
 import Trips from "./all trips/alltrips";
 import Filter from "./filter/filter";
+import Carouselimage from "./Carousel/carousel";
 
 const Home = () => {
   return (
     <div>
-      <Trips />
+      <Carouselimage />
       <Filter />
+
     </div>
   );
 };
