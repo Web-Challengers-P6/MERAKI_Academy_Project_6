a24841ff70bd3021f1576570983b5ecd8f92918f
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-15 07:02:27 +0200

    edit function regester 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import NavBar from "./components/Navbar";
 import Profile from "./components/Profile";
 import NewTrip from "./components/add trip/addtrip";
-// import Login from "./components/login/login";
+import Login from "./components/login/login";
 const App = () => {
   return (
     <>
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/addTrip" element={<NewTrip />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
       </Routes>{" "}
     </>
   );
