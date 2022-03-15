b14522aed77e93a9f8be6a13a57e1f27913c245f
majdinazzal <majdiahmadnazzal@gmail.com>
2022-03-15 22:36:48 +0300

    filter trips

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import AllTrips from "./all trips/alltrips"
-import Trips from "./all trips/alltrips"
-
+import Trips from "./all trips/alltrips";
+import Filter from "./filter/filter";
 
 const Home = () => {
   return (
     <div>
-      <Trips/>
+      <Trips />
+      <Filter />
     </div>
   );
 };
