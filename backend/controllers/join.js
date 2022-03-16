e00100f216b70a57be0b8c6576092714cfadf46b
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-17 00:06:31 +0300

    join trip function

--- backend/controllers/join.js
+++ backend/controllers/join.js
@@ -1,10 +1,24 @@
 //first when we click join we it will send the user(who clicked join ) id
 
+const connection = require("../database/db");
 const db = require("../database/db");
 
 const joinTripFunc = (req, res) => {
-  const riderId = req.params.riderId;
-  
+  const tripId = req.params.tripId;
+  const riderId = req.body.riderId;
+
+  const passengers = [];
+  passengers.push(riderId);
+  console.log(passengers);
+  const query = `insert into rider riderid=${riderId} where tripid=${tripId} `;
+  connection.query(query,(err,result)=>{
+
+if (err) {
+    console.log(err);
+
+}
+
+  })
 };
 
 module.exports = { joinTripFunc };
