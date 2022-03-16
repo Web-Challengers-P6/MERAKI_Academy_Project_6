a8057cb37a62eea38a28e0971d8760d227d1d4a9
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-16 12:48:50 +0300

    accept request

--- backend/controllers/acceptrequest.js
+++ backend/controllers/acceptrequest.js
@@ -2,8 +2,9 @@ const connection = require("../database/db");
 const db = require("../database/db");
 
 const acceptrequest = (req, res) => {
+  let numOfSeats = 0;
   const id = req.params.id;
-  const query = `update trip set acceptrequest=1 where id=?  `;
+  const query = `update trip set numbersite=${(numOfSeats += 1)} where id=?  `;
   const data = [id];
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -14,9 +15,12 @@ const acceptrequest = (req, res) => {
         result: result,
       });
     } else {
-      res
-        .status(200)
-        .json({ success: true, message: " acceptrequest =1", result: result });
+      res.status(200).json({
+        success: true,
+        message: " softDelete =1",
+        numOfSeats: numOfSeats + 1,
+        result: result,
+      });
     }
   });
 };
