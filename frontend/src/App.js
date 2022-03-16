478dbdda933a27f894329bd0660a3cedd0c013b2
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-16 23:51:08 +0300

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,21 @@ import NavBar from "./components/Navbar";
 import Profile from "./components/Profile";
 import NewTrip from "./components/add trip/addtrip";
 import Login from "./components/login/login";
-import MapContainer from "./components/map"
-import Footer from "./components/footer/footer"
+import MapContainer from "./components/map";
+import Footer from "./components/footer/footer";
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/addTrip" element={<NewTrip />} />
         <Route path="/MapContainer" element={<MapContainer />} />
         <Route path="/login" element={<Login />} />
-        
       </Routes>{" "}
-      <Footer/>
+      <Footer />
     </>
   );
 };
