180faa2846c90467a08619334a3bd26e91a2a99a
AbdullahhALammoush <Abdullahh.Alammoush@gmail.com>
2022-03-17 10:32:26 +0300

    join

--- backend/routes/joinAndAccept.js
+++ backend/routes/joinAndAccept.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { joinTripFunc } = require("../controllers/join");
+const { joinTripFunc, update } = require("../controllers/join");
 
 // main route is /join
 const joinRouter = express.Router();
 joinRouter.post("/:tripId", joinTripFunc);
+joinRouter.put("/update/:id", update);
 
 module.exports = { joinRouter };
