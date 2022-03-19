48438aede23f653c4b6490481c798baf097370fc
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-03-19 23:38:55 +0200

    send email

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,113 +1,69 @@
-import React from "react";
-// import { Link } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+// import React from "react";
+// // import { Link } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
 
-import { logout } from "../reducers/loginReducer";
+// import { logout } from "../reducers/loginReducer";
 
-import { Navbar, Container, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Navbar.css";
-//nav
-const NavBar = () => {
-  const state = useSelector((state) => {
-    return { isLoggedIn: state.loginReducer.isLoggedIn };
-  });
-  const dispatch = useDispatch();
-  const token = localStorage.getItem("token");
-  const history = useNavigate();
-  return (
-    <>
+// import { Navbar, Container, Nav } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./Navbar.css";
+// //nav
+// const NavBar = () => {
+//   const state = useSelector((state) => {
+//     return { isLoggedIn: state.loginReducer.isLoggedIn };
+//   });
+//   const dispatch = useDispatch();
+//   const token = localStorage.getItem("token");
+//   const history = useNavigate();
+//   return (
+//     <>
 
-    <Navbar bg="dark" variant="dark">
+//     <Navbar bg="dark" variant="dark">
        
-      <Container>
-      {state.isLoggedIn ? (
-        <> 
-        <Navbar.Brand id="logo" href="home">
-          PickUP
-        </Navbar.Brand>
-        <Nav className="me-auto">
+//       <Container>
+//       {state.isLoggedIn ? (
+//         <> 
+//         <Navbar.Brand id="logo" href="home">
+//           PickUP
+//         </Navbar.Brand>
+//         <Nav className="me-auto">
 
-          <Nav.Link className="navLinks" href="home">
-            Home
-          </Nav.Link>
-          <Nav.Link className="navLinks" href="addTrip">
-            Add Trip
-          </Nav.Link>
-          <Nav.Link className="navLinks" href="trips">
-            Book Trip
-          </Nav.Link>
-          <Nav.Link className="navLinks" href="profile">
-            Profile
-          </Nav.Link>
-          <Nav.Link className="navLinks"  onClick={() => {
-                dispatch(logout());
-                localStorage.clear();
-                history("/login");
-              }} href="">
-            logout
-          </Nav.Link>
-          <Nav.Link className="navLinks" href="MapContainer">
-map          </Nav.Link>
-</Nav>
-</>
-) : ( <>
-          <Nav.Link className="navLinks" href="login">
-            Login
-          </Nav.Link>
-          </>
+//           <Nav.Link className="navLinks" href="home">
+//             Home
+//           </Nav.Link>
+//           <Nav.Link className="navLinks" href="addTrip">
+//             Add Trip
+//           </Nav.Link>
+//           <Nav.Link className="navLinks" href="trips">
+//             Book Trip
+//           </Nav.Link>
+//           <Nav.Link className="navLinks" href="profile">
+//             Profile
+//           </Nav.Link>
+//           <Nav.Link className="navLinks"  onClick={() => {
+//                 dispatch(logout());
+//                 localStorage.clear();
+//                 history("/login");
+//               }} href="">
+//             logout
+//           </Nav.Link>
+//           <Nav.Link className="navLinks" href="MapContainer">
+// map          </Nav.Link>
+// </Nav>
+// </>
+// ) : ( <>
+//           <Nav.Link className="navLinks" href="login">
+//             Login
+//           </Nav.Link>
+//           </>
 
-      <Navbar id="position" bg="dark" variant="dark">
-        <Container>
-          {token ? (
-            <>
-              <Navbar.Brand id="logo" href="home">
-                PickUP
-              </Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link className="navLinks" href="home">
-                  Home
-                </Nav.Link>
-                <Nav.Link className="navLinks" href="addTrip">
-                  Add Trip
-                </Nav.Link>
-                <Nav.Link className="navLinks" href="filter">
-                  Filter
-                </Nav.Link>
-                <Nav.Link className="navLinks" href="contactUs">
-                  Contact Us
-                </Nav.Link>
-                <Nav.Link className="navLinks" href="profile">
-                  Profile
-                </Nav.Link>
-                <Nav.Link
-                  className="navLinks"
-                  onClick={() => {
-                    dispatch(logout());
-                    localStorage.clear();
-                    history("/login");
-                  }}
-                  href=""
-                >
-                  logout
-                </Nav.Link>
-                <Nav.Link className="navLinks" href="MapContainer">
-                  map{" "}
-                </Nav.Link>
-              </Nav>
-            </>
-          ) : (
-            <>
-              <Nav.Link className="navLinks" href="login">
-                Login
-              </Nav.Link>
-            </>
-
-          )}
-        </Container>
-      </Navbar>
-    </>
-  );
-};
-export default NavBar;
+  
+           
+//           )}
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// };
+// export default NavBar;
